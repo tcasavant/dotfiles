2c8c0992a577e65fe90c7f7c3bf80139718e15c8
tcasavant <tsc061003@gmail.com>
2023-03-25 23:28:51 -0400

    Separator plugin for sketchybar

---
+++ sketchybar/plugins/separator.sh
@@ -0,0 +1,15 @@
+BOX_COLOR=0xff282930
+ACCENT_COLOR=0xff84aafa
+
+sketchybar --set $NAME label="|" \
+                       label.color=$ACCENT_COLOR \
+                       icon.color=$ACCENT_COLOR \
+                       background.color=$BAR_COLOR \
+                       background.border_color=$BAR_COLOR \
+                       background.padding_left= 0\
+                       background.padding_right= 0\
+                       label.padding_left=0 \
+                       label.padding_right=4 \
+                       icon.padding_left=0 \
+                       icon.padding_right=0 \
+                       align="left" \
